f5aa23fb6fe11ab7d5a110d2a7c2fb6a18d747ed
Bruno Galvao <brunoqgalvao@hotmail.com>
2019-05-24 17:12:41 -0300

    brindeira elemento

--- index.php
+++ index.php
@@ -1,27 +1,193 @@
+ <?php
+    $usuario = ["logado"=>false];
+    //$usuario = ["logado" => true, nome" => "Bruno Galvao", "nivelAcesso" => 0];
+ ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Virtual PHP</title>
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+  </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+  </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+  </script>
 </head>
+
 <body>
-   <?php 
-    $nome = "Vinicius";
-    
-    // ola mundo
-      echo $nome;
-      if($nome == "Vinicius"){
-        echo "Ok - é Vinicius";
-      }else{
-        echo "Não é Vinicius";
-      }
-      for ($i=0; $i < 5; $i++) { 
-        echo "<h1> $i </h1>";
-      }
-      var_dump($nome)
-    ?>
+
+  <header>
+    <!-- NAVBAR DO BOOSTRAP -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#"> Cursos </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+
+          <!-- php tags forma aberta-->
+          <?php if($usuario['nivelAcesso'] == 0): ?>
+            <li class="nav-item active">
+              <a class="nav-link" href="#"> Perfil <span class="sr-only">(current)</span></a>
+            </li>
+          <?php else: ?>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Editar Conteúdo<span class="sr-only">(current)</span></a>
+            </li>
+          <?php endif ?>
+          <!-- php tags, logica de elemento -->
+          <?php
+          
+            $elemOlaUsuario = "
+            <li class='nav-item'>
+              <a href='#' class='nav-link'> Olá {$usuario['nome']} </a>
+            </li>
+            ";
+            $elemLogin = "
+            <li class='nav-item'>
+              <a href='#' class='nav-link'> Login </a>
+            </li>
+            ";
+            echo $usuario[logado]? $elemOlaUsuario : $elemLogin;
+          ?>
+          
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <!-- container define que está num container fixo e mantém centralizado. container-fluid ele ocupa tudo -->
+  <main class="container mt-5">
+    <section class="row">
+      <div class="col-md-4">
+        <!-- coluna para segurar card -->
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <!-- coluna para segurar card -->
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <!-- coluna para segurar card -->
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="row">
+      <div class="col-md-4">
+        <!-- coluna para segurar card -->
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <!-- coluna para segurar card -->
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <!-- coluna para segurar card -->
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="row">
+      <div class="col-md-4">
+        <!-- coluna para segurar card -->
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <!-- coluna para segurar card -->
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <!-- coluna para segurar card -->
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+
+
 </body>
 
 </html>
